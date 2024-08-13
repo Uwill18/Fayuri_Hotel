7162c1b015e04f888d177243ae4d716645fcdff8
UWILL18 <uwill18@wgu.edu>
2024-08-13 10:56:35 -0500

    fayuri hotel commences

---
+++ src/main/java/Hotel/Booking/example/demo/Fayuri_Application.java
@@ -0,0 +1,13 @@
+package Hotel.Booking.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fayuri_Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fayuri_Application.class, args);
+	}
+
+}
---
+++ src/test/java/Hotel/Booking/example/demo/HotelBookingAppApplicationTests.java
@@ -0,0 +1,17 @@
+package Hotel.Booking.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//Luke 4:10
+//Deuteronomy 31:8
+//Ecclesiastes 3:4
+
+@SpringBootTest
+class HotelBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
